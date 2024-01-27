234c1b31cef2e1476cc5607dfdfa2c61b180d6ef
Suryanshkapoor <suryanshkapoorji@gmail.com>
2024-01-27 09:19:38 +0530

    Update

--- src/components/LeftSection.js
+++ src/components/LeftSection.js
@@ -19,19 +19,19 @@ import {
 		<div className="flex h-full min-h-0 flex-col">
 		  <div className="flex h-full w-full flex-1 items-start border-white/20">
 			<nav className="flex h-full flex-1 flex-col space-y-1 p-2">
-			  <a className="flex px-3 py-3 items-center gap-3 rounded-md text-white cursor-pointer border border-white/20 text-sm mb-2 flex-shrink-0 hover:bg-gray-500/10">
+			  <span className="flex px-3 py-3 items-center gap-3 rounded-md text-white cursor-pointer border border-white/20 text-sm mb-2 flex-shrink-0 hover:bg-gray-500/10">
 				<PlusIcon />
 				New Chat
-			  </a>
+			  </span>
 			  <div className="flex-col flex-1 overflow-y-auto border-b border-white/20"></div>
 			  {options.map((item) => (
-				<a
+				<span href="#"
 				  className="flex gap-3 px-3 py-3 items-center rounded-md hover:bg-gray-500/10 text-white cursor-pointer"
 				  key={item.text}
 				>
 				  {item.icon}
 				  {item.text}
-				</a>
+				</span>
 			  ))}
 			</nav>
 		  </div>
